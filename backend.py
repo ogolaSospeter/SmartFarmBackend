89be7d5c514544af15d23af62cf7e0944a8d3e44
Ogola Sospeter <okongo.ogola1@students.jkuat.ac.ke>
2025-04-10 17:43:02 +0300

    Validating image on upload

--- backend.py
+++ backend.py
@@ -28,7 +28,6 @@ genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 model = genai.GenerativeModel("gemini-2.0-flash")
 
 def get_tomato_disease_recommendations(disease, temperature, moisture):
-    print(f"Getting recommendations for {disease} with temperature {temperature}°C and moisture {moisture}Hg.")
 
     _system_instruction = (
         "You are SmartFarm, an expert in tomato farming. "
@@ -105,12 +104,10 @@ def send_prompt_to_gemini(user_id, user_prompt):
 
     chat = chat_sessions[user_id]
 
-    print(f"Sending prompt to Gemini AI: {user_prompt}")
-
     try:
         response = chat.send_message(user_prompt)  # Uses chat session memory
         newtext = response.text if hasattr(response, "text") and response.text else "I'm not sure how to respond."
-        print("The received response was: " + newtext)
+        
 
     except Exception as e:
         return {"message": f"Error: {str(e)}"}
@@ -119,10 +116,10 @@ def send_prompt_to_gemini(user_id, user_prompt):
 
 @app.route('/chat/<user_id>/<message>', methods=['GET'])
 def chat(user_id, message):
-    print(f"Received message from user {user_id}: {message}")
+
     user_message = message.strip()
     bot_reply = send_prompt_to_gemini(user_id, user_message)
-    print(f"Sending reply: {bot_reply['message']}")
+   
     return jsonify(bot_reply)
 
 """
@@ -153,8 +150,7 @@ def generate_management_practises():
     
 @app.route('/managementpractises', methods=['GET'])
 def get_management_practises():
-    # print("Generating management practises")
-    # print("Generated practices: \n{}".format(generate_management_practises()))
+  
     return generate_management_practises()
 
 # Integration of the Model for Image classification.
@@ -209,7 +205,8 @@ def classify_image(image_data):
 
         # Get the classification result
         output_data = interpreter.get_tensor(output_details[0]['index'])
-        predicted_class = np.argmax(output_data[1])
+        print("\n\nOutput data: ", output_data)
+        predicted_class = np.argmax(output_data[0])
 
         return labels[predicted_class]  # Return the label of the predicted class
     except Exception as e:
