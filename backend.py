1adbb9eac33345ab5908a64f21bb3628617bbb70
Ogola Sospeter <okongo.ogola1@students.jkuat.ac.ke>
2025-04-11 10:57:49 +0300

    Image Validation

--- backend.py
+++ backend.py
@@ -186,29 +186,30 @@ def classify_image(image_data):
         if not is_leaf_image(image_data):
             print("Invalid image: Not a leaf image")
             return "Error: The provided image is not a valid tomato leaf."
-    # Preprocess the image to fit EfficientNet input
-        image = Image.open(BytesIO(image_data))
-        image = image.resize((224, 224))  # EfficientNet expects 224x224 images
-        image = np.array(image).astype(np.float32)
-        image = np.expand_dims(image, axis=0)
-        # Normalize the image (efficientnet expects this preprocessing)
-        image = image / 255.0
-        # Get model input details
-        input_details = interpreter.get_input_details()
-        output_details = interpreter.get_output_details()
-        print("\n\nInput details: ", input_details)
-        print("\n\nOutput details: ", output_details)
+        else:
+        # Preprocess the image to fit EfficientNet input
+            image = Image.open(BytesIO(image_data))
+            image = image.resize((224, 224))  # EfficientNet expects 224x224 images
+            image = np.array(image).astype(np.float32)
+            image = np.expand_dims(image, axis=0)
+            # Normalize the image (efficientnet expects this preprocessing)
+            image = image / 255.0
+            # Get model input details
+            input_details = interpreter.get_input_details()
+            output_details = interpreter.get_output_details()
+            print("\n\nInput details: ", input_details)
+            print("\n\nOutput details: ", output_details)
 
-        # Set the input tensor
-        interpreter.set_tensor(input_details[0]['index'], image)
-        interpreter.invoke()
+            # Set the input tensor
+            interpreter.set_tensor(input_details[0]['index'], image)
+            interpreter.invoke()
 
-        # Get the classification result
-        output_data = interpreter.get_tensor(output_details[0]['index'])
-        print("\n\nOutput data: ", output_data)
-        predicted_class = np.argmax(output_data[0])
+            # Get the classification result
+            output_data = interpreter.get_tensor(output_details[0]['index'])
+            print("\n\nOutput data: ", output_data)
+            predicted_class = np.argmax(output_data[0])
 
-        return labels[predicted_class]  # Return the label of the predicted class
+            return labels[predicted_class]  # Return the label of the predicted class
     except Exception as e:
         print("\n\nError in image classification: ", str(e))
         return "Error in classification : " + str(e)
