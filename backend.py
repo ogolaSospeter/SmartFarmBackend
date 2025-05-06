cf6a9531b31dfc72da64f3c5fe61de447b87f0f9
Ogola Sospeter <okongo.ogola1@students.jkuat.ac.ke>
2025-05-06 10:05:50 +0300

    Classify Image refactor

--- backend.py
+++ backend.py
@@ -161,9 +161,9 @@ def classify_image(image_data):
         print("\n\nClassifying image...")
     # Preprocess the image to fit EfficientNet input
         image = Image.open(BytesIO(image_data)).convert("RGB")
-        # image = image.resize((224, 224))  # EfficientNet expects 224x224 images
-        # image = np.array(image).astype(np.float32)
-        # image = np.expand_dims(image, axis=0)
+        image = image.resize((224, 224))  # EfficientNet expects 224x224 images
+        image = np.array(image).astype(np.float32)
+        image = np.expand_dims(image, axis=0)
         # # Normalize the image (efficientnet expects this preprocessing)
         # image = image / 255.0
         # Get model input details
